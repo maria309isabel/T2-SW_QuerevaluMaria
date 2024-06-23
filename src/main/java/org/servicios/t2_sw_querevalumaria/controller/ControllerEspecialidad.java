1b9e624b294d6a9aaaf38494d29df477f3369ec5
maria309isabel <isa.mar.xv@gmail.com>
2024-06-23 12:31:43 -0500

    Terminado ApiEspecialidad controller, service, serviceImpl

--- src/main/java/org/servicios/t2_sw_querevalumaria/controller/ControllerEspecialidad.java
+++ src/main/java/org/servicios/t2_sw_querevalumaria/controller/ControllerEspecialidad.java
@@ -1,15 +1,52 @@
 package org.servicios.t2_sw_querevalumaria.controller;
 
+import org.servicios.t2_sw_querevalumaria.model.Especialidad;
 import org.servicios.t2_sw_querevalumaria.service.EspecialidadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RestController
+@RequestMapping("/api/v1")
 
 public class ControllerEspecialidad {
+
+    @Autowired
+    private EspecialidadService especialidadService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Especialidad>> findAll() {
+
+        return new ResponseEntity<>(especialidadService.findAll(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/listarPorId/{idEspecialidad}")
+
+    public ResponseEntity<Especialidad> findById(@PathVariable Integer idEspecialidad) {
+
+        return new ResponseEntity<>(especialidadService.findById(idEspecialidad), HttpStatus.OK);
+
+    }
+
+    @PostMapping("/agregarEspecialidad")
+
+    public ResponseEntity<Especialidad> agregarEspecialidad(@RequestBody Especialidad especialidad) {
+
+        return new ResponseEntity<>(especialidadService.agregarEspecialidad(especialidad), HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/actualizarEspecialidad")
+
+    public ResponseEntity<Especialidad> actualizarEspecialidad (@RequestBody Especialidad especialidad) {
+
+        return new ResponseEntity<>(especialidadService.actualizarEspecialidad(especialidad), HttpStatus.OK);
+
+    }
+
+}
 
