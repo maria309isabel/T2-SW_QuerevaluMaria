075d33d0081ec89b616d26e530734393fdf416ac
maria309isabel <isa.mar.xv@gmail.com>
2024-06-23 11:45:55 -0500

    Empezando Pregunta 2

---
+++ src/main/java/org/servicios/t2_sw_querevalumaria/T2SwQuerevaluMariaApplication.java
@@ -0,0 +1,13 @@
+package org.servicios.t2_sw_querevalumaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwQuerevaluMariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2SwQuerevaluMariaApplication.class, args);
+    }
+
+}
