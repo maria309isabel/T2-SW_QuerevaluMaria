3280a55c39878a1613217bd2a8213612c15a3bac
maria309isabel <isa.mar.xv@gmail.com>
2024-06-23 12:48:04 -0500

    Empezando pregunta 4

---
+++ src/main/java/org/servicios/t2_sw_querevalumaria/dto/ArchivoDto.java
@@ -0,0 +1,13 @@
+package org.servicios.t2_sw_querevalumaria.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+
+public class ArchivoDto {
+
+    private String mensaje;
+
+}
---
+++ src/main/java/org/servicios/t2_sw_querevalumaria/dto/EntityDto.java
@@ -0,0 +1,4 @@
+package org.servicios.t2_sw_querevalumaria.dto;
+
+public class EntityDto {
+}
